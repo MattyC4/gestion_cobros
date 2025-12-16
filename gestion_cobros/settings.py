62d92d3627305c6119361dfe26d8cfac0bda4a47
sCubiid . <kaushxoficial@gmail.com>
2025-12-16 15:25:27 -0300

    settings.py link

--- gestion_cobros/settings.py
+++ gestion_cobros/settings.py
@@ -120,7 +120,7 @@ DATABASES = {
 CSRF_TRUSTED_ORIGINS = [
     'http://127.0.0.1:8000',
     'http://localhost:8000',
-    'https://gestioncobros-production.up.railway.app'
+    'https://flowweb-production.up.railway.app'
 ]
 
 # settings.py
